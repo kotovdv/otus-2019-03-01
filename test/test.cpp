d589b2bc25b6b6ca575ccda13fc77daa266c10c3
kotovdv <kotovdv@yandex-team.ru>
2019-03-28 01:09:29 +0300

    Proper naming fix

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,9 @@
-#define BOOST_TEST_MODULE hello_world_test_module
+#define BOOST_TEST_MODULE helloworld_test_module
 
 #include "../lib/app_version.h"
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(hello_world_test_suite)
+BOOST_AUTO_TEST_SUITE(helloworld_test_suite)
 
     BOOST_AUTO_TEST_CASE(majorVersionIsCorrect) {
         BOOST_CHECK(majorVersion() == "9");
